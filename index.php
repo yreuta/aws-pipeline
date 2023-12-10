639631dc548a7dd3b5a7581f958b5ff761012fd7
yevhenreuta <r3utaa@gmail.com>
2023-12-10 19:52:15 +0200

    changed index.php

--- index.php
+++ index.php
@@ -1,45 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Test Results</title>
+    <title>XML Data</title>
     <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-        th, td {
+        pre {
+            background-color: #f4f4f4;
             border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
+            padding: 10px;
+            overflow-x: auto;
         }
     </style>
 </head>
 <body>
 
 <?php
-    $xmlData = simplexml_load_file("pytest_reports/pytest_report.xml") or die("Unable to load the XML.");
-
-    if ($xmlData) {
-        echo '<table>';
-        echo '<tr><th>Test Name</th><th>Status</th></tr>';
-        
-        foreach ($xmlData->testcase as $test) {
-            $testName = (string)$test['classname'] . '::' . (string)$test['name'];
-            $testStatus = ($test->failure) ? 'Failed' : 'Passed';
-
-            echo '<tr>';
-            echo '<td>' . $testName . '</td>';
-            echo '<td>' . $testStatus . '</td>';
-            echo '</tr>';
-        }
-        
-        echo '</table>';
-    } else {
-        echo 'No data available';
-    }
+    $xmlData = simplexml_load_file("pytest_reports/pytest_report.xml") 
+        or die("Unable to load the xml.");
+    echo '<pre>';
+    print_r($xmlData);
+    echo '</pre>';
 ?>
 
 </body>
